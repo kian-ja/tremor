d70dd3773da5056215dccb7e8f1b020dd3aee300
Kian jalaleddini <jalaledd@usc.edu>
2016-08-04 16:18:42 -0700

    identify stretch reflex magnitude to pulse perturbation

---
+++ pulseReflexAmplitude.m
@@ -0,0 +1,27 @@
+function reflexAmplitude = pulseReflexAmplitude(position,torque,threshold,reflexAmplitude)
+positionDiff = diff(position);
+positionDiff = max(positionDiff,0);
+largeVelocityIndex = find(positionDiff>threshold);
+largeVelocityIndexDiff = diff(largeVelocityIndex);
+falseJumpIndex = find(largeVelocityIndexDiff == 1);
+falseJumpIndex = falseJumpIndex + 1;
+largeVelocityIndex(falseJumpIndex) = [];
+reflexOnsetIndex = largeVelocityIndex + 40;
+reflexEndIndex = largeVelocityIndex + 640;
+numPulses = length(reflexOnsetIndex);
+reflexAmplitudeThisTrial = zeros(numPulses,1);
+figure(1000)
+time = 1 : length(position);
+time = time * 0.001;
+plot(time,torque)
+hold on
+for i = 1 : numPulses
+     response = torque(reflexOnsetIndex(i):reflexEndIndex(i));
+     plot(time(reflexOnsetIndex(i):reflexEndIndex(i)),torque(reflexOnsetIndex(i):reflexEndIndex(i)),'o')
+     responseSteadyState = mean(response(400:end));
+     response = response - responseSteadyState;
+     reflexAmplitudeThisTrial(i) = sum(response.^2)/length(response);
+end
+pause
+close(1000)
+reflexAmplitude = [reflexAmplitude;reflexAmplitudeThisTrial];
--- stretchReflexAnalysis.m
+++ stretchReflexAnalysis.m
@@ -2,7 +2,35 @@ load stretchReflexTrials
 numSubject = length(experiment.PF2.MVC{1});
 for i = 1 : numSubject
     pulseTrials = experiment.DF.pulseTrials{i};
+    reflexAmplitude = [];
     for j = 1 : length(pulseTrials)
+        position = flbReadDesiredPosition(experiment.fileName{i},pulseTrials(j));
         torque = flbReadTorque(experiment.fileName{i},pulseTrials(j));
+        reflexAmplitude = pulseReflexAmplitude(position,torque,0.01,reflexAmplitude); 
     end
+    results.DF.pulseReflexAmplitude.mean = mean(reflexAmplitude);
+    results.DF.pulseReflexAmplitude.std = std(reflexAmplitude);
+    
+    
+    pulseTrials = experiment.PF1.pulseTrials{i};
+    reflexAmplitude = [];
+    for j = 1 : length(pulseTrials)
+        position = flbReadDesiredPosition(experiment.fileName{i},pulseTrials(j));
+        torque = flbReadTorque(experiment.fileName{i},pulseTrials(j));
+        reflexAmplitude = pulseReflexAmplitude(position,torque,0.01,reflexAmplitude); 
+    end
+    results.PF1.pulseReflexAmplitude.mean = mean(reflexAmplitude);
+    results.PF1.pulseReflexAmplitude.std = std(reflexAmplitude);
+    
+    
+    pulseTrials = experiment.PF2.pulseTrials{i};
+    reflexAmplitude = [];
+    for j = 1 : length(pulseTrials)
+        position = flbReadDesiredPosition(experiment.fileName{i},pulseTrials(j));
+        torque = flbReadTorque(experiment.fileName{i},pulseTrials(j));
+        reflexAmplitude = pulseReflexAmplitude(position,torque,0.01,reflexAmplitude); 
+    end
+    results.PF2.pulseReflexAmplitude.mean = mean(reflexAmplitude);
+    results.PF2.pulseReflexAmplitude.std = std(reflexAmplitude);
 end
+save stretchReflexResults results
