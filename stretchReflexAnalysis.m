a6151626a1b6ebded5e2ae2c3987d789567a9dff
Kian jalaleddini <jalaledd@usc.edu>
2016-08-04 15:26:29 -0700

    reading torque for pulse perturbation

--- stretchReflexAnalysis.m
+++ stretchReflexAnalysis.m
@@ -1,8 +1,8 @@
 load stretchReflexTrials
 numSubject = length(experiment.PF2.MVC{1});
 for i = 1 : numSubject
-    numPulseTrials = length(experiment.DF.pulseTrials{i});
-    for j = 1 : numPulseTrials
-        
+    pulseTrials = experiment.DF.pulseTrials{i};
+    for j = 1 : length(pulseTrials)
+        torque = flbReadTorque(experiment.fileName{i},pulseTrials(j));
     end
 end
